16269052d277c8f4aae870066f82d3494fda7563
Kagga Devi Sri <devisrikagga09@gmail.com>
2026-09-03 20:05:01 +0530

    notes update

--- backend/app.js
+++ backend/app.js
@@ -6,12 +6,14 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-
-
-
 const app = express();
 const noteRoutes = require("./routes/noteRoutes");
-app.use(cors());
+
+app.use(cors({
+    origin: "https://campus-connect-hub-theta.vercel.app",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 app.use("/api/notes", noteRoutes);
